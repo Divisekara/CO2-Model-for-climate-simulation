da0a12d62237ecba4dc5651b1f0ffa21ec1e2dec
Divisekara <9asitha7@gmail.com>
2020-05-20 17:08:13 +0530

    4

--- CO2_1.m
+++ CO2_1.m
@@ -1,3 +1,4 @@
+clc,clear
 %%
 
 % A CO2 model for climate simulation
@@ -7,26 +8,31 @@
 %variables
 
 %%five principal variables
-% p = Partial pressure of carbo dioxide in the atmosphere
-% sigma_s = total dissolved carbon concentration in the shallow ocean
-% sigma_d = total dissolved carbon concentration in the deep ocean
-% alpha_s = alkalinity in the shallow ocean
-% alpha_d = alkalinity in the deep ocean
+% p =                         Partial pressure of carbo dioxide in the atmosphere
+% sigma_s =                   total dissolved carbon concentration in the shallow ocean
+% sigma_d =                   total dissolved carbon concentration in the deep ocean
+% alpha_s =                   alkalinity in the shallow ocean
+% alpha_d =                   alkalinity in the deep ocean
 
-% h_s = hudrogen carbonate in the shallow ocean
-% c_s = carbonate in the shallow ocean ocean
-% p_s = partial pressure of gaseous carbon dioxide in the shallow ocean
+% h_s =                       %hudrogen carbonate in the shallow ocean
+% c_s =                        carbonate in the shallow ocean ocean
+% p_s =                         partial pressure of gaseous carbon dioxide in the shallow ocean
 
-% d = transfer time
 % source term f(t)
-% u_1 = 
-% u_2 = 
-% v_s = 
-% v_d =
-% w = 
-% k1 = 
-% k2 = 
 
+%contants
+% d = 8.64;                       transfer time
+% u_1 = 4.95*10^2;
+% u_2 = 4.95*10^(-2);
+% v_s = 0.12;
+% v_d = 1.23;
+% w   = 10^(-3);
+% k1  = 2.19*10^(-4);
+% k2  = 6.12*10^(-5);
+% k3  = 0.997148;
+
+%Time interval is thousand years agon and few thousand years ahead
+t_interval = fixed.Interval(1000) 
 %%
 
 %The rate of change of the five principal variables
@@ -42,6 +48,22 @@
 % alpha_d_derivative = (k2 - (alpha_d - alpha_s)*w)/v_d
 
 
-%The equlibrium between carbon dioxide and the corbonates 
+% The equlibrium between carbon dioxide and the corbonates 
+
+% h_s = (sigma_s - sqrt(sigma_s^2 - k3*alpha_s*(s*sigma_s - alpha_s)))/k3 
+
+% c_s = (alpha_s - h_s)/2
+
+% p_s = k4 * h_s^2 /c_s
+
+
+
 
 %%
+
+
+
+
+
+
+
