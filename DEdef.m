d77d3853706ed881733d1765667660bedf5fe57f
Divisekara <9asitha7@gmail.com>
2020-05-21 22:23:37 +0530

    2

---
+++ DEdef.m
@@ -0,0 +1,21 @@
+function [p_derivative] = DEdef(I, D)
+    %Independent variable = t
+    
+    d = 8.64;                %transfer time
+    u_1 = 4.95*10^2;
+    u_2 = 4.95*10^(-2);
+    v_s = 0.12;
+    v_d = 1.23;
+    w   = 10^(-3);
+    k1  = 2.19*10^(-4);
+    k2  = 6.12*10^(-5);
+    k3  = 0.997148;
+    
+    p =
+    sigma_s =
+    sigma_d =
+    alpha_s =     
+    alpha_d =
+
+    p_derivative = (p_s - p)/(d) + f(t)/u_1; 
+end
--- p_derivative_equation.m
+++
@@ -1,3 +0,0 @@
-function [p_derivative] = p_derivative_equation(p_s,p,f(t))
-    p_derivative = (p_s - p)/(d) + f(t)/u_1; 
-end
